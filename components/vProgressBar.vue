45112a1b86855b854c86a12af4c18a33ddaf912f
Anna Avdalyan <anna_avdalyan@intdevels.com>
2024-12-25 20:38:24 +0400

    fix

--- components/vProgressBar.vue
+++ components/vProgressBar.vue
@@ -1,23 +1,11 @@
-<template>
-  <div class="card">
-    <Toast></Toast>
-    <ProgressBar :value="value1"/>
-    <div class="progress-text">
-      <span>Fitting in progress...</span>
-      <div class="progress">{{value1}} %</div>
-    </div>
-  </div>
-</template>
 
 <script setup>
 import {ref, onMounted, onBeforeUnmount} from "vue";
-import {useToast} from "primevue/usetoast";
 
 onMounted(() => startProgress())
 
 onBeforeUnmount(() => endProgress())
 
-const toast = useToast();
 const value1 = ref(0);
 const interval = ref();
 const startProgress = () => {
@@ -35,3 +23,13 @@ const endProgress = () => {
   interval.value = null;
 };
 </script>
+<template>
+  <div class="card">
+    <ProgressBar :value="value1"/>
+    <div class="progress-text">
+      <span>Fitting in progress...</span>
+      <div class="progress">{{value1}} %</div>
+    </div>
+  </div>
+</template>
+
